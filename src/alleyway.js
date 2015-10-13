818536e92b0edb2f86ae7fa95119a2bdab59805b
ddchen <842914439@qq.com>
2015-10-13 11:42:53 +0800

    add grammer * and split alleyway code.

--- src/alleyway.js
+++ src/alleyway.js
@@ -8,12 +8,9 @@
  */
 
 import ast from "expressioner";
-import parallel from "./parallel";
-import compose from "./compose";
-import iterate from "./iterate"
-import serial from "./serial";
-import high from "./high";
 import packer from "./packer"
+import operation from "./operation";
+import getOperateValues from "./getOperateValues";
 
 var defineUnit = (name, method, funMap, operationMap) => {
     // check name
@@ -31,103 +28,9 @@ var defineUnit = (name, method, funMap, operationMap) => {
     }
 }
 
-var getValue = (name, funMap, valueMap) => {
-    let fun = name;
-    if (typeof name === "string") {
-        if (valueMap.hasOwnProperty(name)) {
-            fun = valueMap[name];
-        } else {
-            fun = funMap[name];
-        }
-    }
-    if (!fun) throw new Error("missing definition for function " + name);
-    return fun;
-}
-
-var getOperateValues = (y, funMap, valueMap) => {
-    let vs = [];
-    for (let i = 0; i < y.length; i++) {
-        let name = y[i];
-        let fun = getValue(name, funMap, valueMap);
-        vs.push(fun);
-    }
-    return vs;
-}
-
-var generateOperationExecutor = (operationMap, funMap, valueMap) => {
-    operationMap[","].execute = (...y) => {
-        let vs = getOperateValues(y, funMap, valueMap);
-        let fun1 = vs[0];
-        let fun2 = vs[1];
-        return parallel(fun1, fun2);
-    }
-    operationMap["~"].execute = (...y) => {
-        let vs = getOperateValues(y, funMap, valueMap);
-        let fun1 = vs[0];
-        let fun2 = vs[1];
-        return serial(fun1, fun2);
-    }
-    operationMap["!"].execute = (...y) => {
-        let vs = getOperateValues(y, funMap, valueMap);
-        let fun = vs[0];
-        return high(fun);
-    }
-    operationMap["|"].execute = (...y) => {
-        let vs = getOperateValues(y, funMap, valueMap);
-        let fun1 = vs[0];
-        let fun2 = vs[1];
-        return compose(fun1, fun2);
-    }
-    operationMap[">"].execute = (...y) => {
-        let vs = getOperateValues(y, funMap, valueMap);
-        let fun1 = vs[0];
-        let fun2 = vs[1];
-        return iterate(fun1, fun2);
-    }
-    operationMap[":"].execute = (left, right) => {
-        let vs = getOperateValues([left], funMap, valueMap);
-        left = vs[0];
-        valueMap[right] = left;
-        return left;
-    }
-}
-
-let operationMap = {
-    ",": {
-        priority: 10,
-        opNum: 2
-    },
-    "~": {
-        priority: 15,
-        opNum: 2
-    },
-    "!": {
-        priority: 30,
-        opNum: 1
-    },
-    "|": {
-        priority: 20,
-        opNum: 2
-    },
-    ">": {
-        priority: 20,
-        opNum: 2
-    },
-    ":": {
-        priority: 20,
-        opNum: 2
-    },
-    "(": {
-        type: "start"
-    },
-    ")": {
-        type: "close",
-        match: "("
-    }
-};
-
 export default (setMap = {}) => {
     let funMap = {};
+    let operationMap = operation.operationMap;
 
     // init
     for (let name in setMap) {
@@ -163,7 +66,7 @@ export default (setMap = {}) => {
 
     var translate = (str) => {
         let valueMap = {};
-        generateOperationExecutor(operationMap, funMap, valueMap);
+        operation.generateOperationExecutor(operationMap, funMap, valueMap);
         return composeSentences(str, valueMap);
     }
 
--- test/iterate.js
+++ test/iterate.js
@@ -44,4 +44,45 @@ describe("iterate", () => {
         assert.equal(res[1], 2);
         done();
     });
+    it("continuous", async(done) => {
+        let op = alleway({
+            "add": (a, b) => new Promise((r) => {
+                setTimeout(() => {
+                    r(a + b);
+                }, 10);
+            }),
+            "sub": (a, b) => new Promise((r) => {
+                setTimeout(() => {
+                    r(a - b);
+                }, 10);
+            }),
+            "dou": v => v * 2,
+            "div": v => v / 4
+        });
+        let v = op.translate("(add, sub) > dou > div");
+        var res = await v(5, 4);
+        assert.equal(res[0], 4.5);
+        assert.equal(res[1], 0.5);
+        done();
+    });
+    it("map & reduce", async(done) => {
+        let op = alleway({
+            "add": (a, b) => new Promise((r) => {
+                setTimeout(() => {
+                    r(a + b);
+                }, 10);
+            }),
+            "sub": (a, b) => new Promise((r) => {
+                setTimeout(() => {
+                    r(a - b);
+                }, 10);
+            }),
+            "dou": v => v * 2,
+            "mul": (a, b) => a * b
+        });
+        let v = op.translate("(add, sub) > dou | mul");
+        var res = await v(5, 4);
+        assert.equal(res[0], 36);
+        done();
+    });
 });
